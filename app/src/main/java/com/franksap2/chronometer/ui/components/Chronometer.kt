065444a1714802e7699777c343801a1483a5e4f4
franksap2 <franksaponaro2@gmail.com>
2022-09-10 00:17:00 +0200

    adjust dial color

--- app/src/main/java/com/franksap2/chronometer/ui/components/Chronometer.kt
+++ app/src/main/java/com/franksap2/chronometer/ui/components/Chronometer.kt
@@ -9,13 +9,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.center
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -24,6 +28,8 @@ import com.franksap2.chronometer.ui.theme.ChronometerComposeTheme
 
 @Composable
 fun Chronometer() {
+
+    val surfaceColor = MaterialTheme.colors.surface
 
     val test = remember {
         Animatable(0f)
@@ -42,21 +48,25 @@ fun Chronometer() {
         Canvas(
             modifier = Modifier.fillMaxSize(1f),
             onDraw = {
-                drawDial(test.value)
+                drawDial(test.value, surfaceColor)
             },
         )
     }
 
 }
 
-fun DrawScope.drawDial(value: Float) {
+fun DrawScope.drawDial(value: Float, circleColor: Color) {
     rotate(value) {
         drawLine(
             color = Amber700.copy(0.8f),
-            start = size.center,
-            end = Offset(size.width / 2, 0f),
-            strokeWidth = 3.dp.toPx()
+            start = size.center.copy(y = size.height * 0.6f),
+            end = Offset(size.width / 2, size.height * 0.1f),
+            strokeWidth = 3.dp.toPx(),
+            cap = StrokeCap.Round
         )
+
+        drawCircle(circleColor, 5.dp.toPx(), size.center)
+        drawCircle(Amber700, 5.dp.toPx(), size.center, style = Stroke(width = 2.dp.toPx()))
     }
 
 }
